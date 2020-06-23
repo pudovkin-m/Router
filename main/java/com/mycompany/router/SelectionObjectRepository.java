76906668bb82a45c7b7d1d8bc2db290786688622
pudovkin-m <61505381+pudovkin-m@users.noreply.github.com>
2020-06-23 18:19:02 +0400

    Update SelectionObjectRepository.java

--- main/java/com/mycompany/router/SelectionObjectRepository.java
+++ main/java/com/mycompany/router/SelectionObjectRepository.java
@@ -7,19 +7,4 @@ public interface SelectionObjectRepository<Entity extends SelectionObject> {
     
     List<Entity> getSelectionPriceLess(Integer threshold, Comparator<Entity> comp);
 
-    default String wcardTest(final int models) {
-        if (models == 0) {
-            return "";
-        } else if (models == 1) {
-            return "AND m.name = (?) ";
-        } else {
-            StringBuilder sb = new StringBuilder();
-            sb.append(" AND (m.name = (?) ");
-            for (int i = 0; i < models - 1; i++) {
-                sb.append(" OR m.name = (?) ");
-            }
-            sb.append(") ");
-            return sb.toString();
-        }
-    }
 }
